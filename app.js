58133dc1b5378602df1419571fdb9a370f8f50df
Tari <bota.maral@gmail.com>
2021-10-15 17:57:30 -0400

    all done

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+const { writeFile, copyFile } = require('./utils/generate-site.js');
+
 const inquirer = require("inquirer");
-const fs = require("fs");
 const generatePage = require("./src/page-template.js");
 
 const promptUser = () => {
@@ -41,14 +42,9 @@ const promptUser = () => {
       type: "input",
       name: "about",
       message: "Provide some information about yourself:",
-      when: ({ confirmAbout }) => {
-        if (confirmAbout) {
-          return true;
-        } else {
-          return false;
-        }
-      },
-    },
+      when: ({ confirmAbout }) => confirmAbout
+    
+    }
   ]);
 };
 
@@ -141,13 +137,19 @@ const promptProject = (portfolioData) => {
 
 promptUser()
   .then(promptProject)
-  .then((portfolioData) => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile("./index.html", pageHTML, (err) => {
-      if (err) throw new Error(err);
-      console.log(
-        "Page created! Check out index.html in this directory to see it!"
-      );
-    });
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
